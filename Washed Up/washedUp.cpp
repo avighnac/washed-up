dd6045420a14162f70edf49c3ff9d2968080994f
Avighna <avighnakc@gmail.com>
2021-11-06 22:29:25 +0530

    BUGFIX: Fixed a bug with the collision detection system which prevented it from working, FINALLY.

--- Washed Up/washedUp.cpp
+++ Washed Up/washedUp.cpp
@@ -43,7 +43,7 @@ Input input = {};
 
 int playerX = 0;
 int playerY = 0;
-float size = buffer_height / 6;
+int size = buffer_height / 6;
 float speed = 5;
 
 int beachCoordinates = 0, wasteCoordinatesX = 0;
@@ -141,7 +141,7 @@ static void erase_element_at_pos(std::vector<Sprite>& vec, size_t position) {
   vec = answer;
 }
 
-bool isInBetween(int start, int end, int val) {
+bool isInContact1D(int start, int end, int val) {
   int tempEnd;
   if (start > end) {
     tempEnd = end;
@@ -152,36 +152,46 @@ bool isInBetween(int start, int end, int val) {
   return start <= val && val <= end;
 }
 
-bool isInContact(int a, int b, int c, int d, int wasteCoordinatesX,
-                 Sprite spr) {
-  spr.startX += wasteCoordinatesX; /*This is done
-                                   because the beach is 
-                                   moving and we have to
-                                   keep track of waste coordinates
-                                   accordingly.*/
-
-  auto cond1 =
-      (isInBetween(a, c, spr.startX) &&
-       isInBetween(b, d, spr.startY));
-  auto cond2 = (isInBetween(spr.startX, spr.endX, a) &&
-                isInBetween(spr.startY, spr.endY, b));
-
-  auto condition = cond1 || cond2;
-
-  if (condition) {
-    spr.startX = spr.startX;
-  }
-
-  return condition;
+void swap(int& a, int& b) {
+  int temp = a;
+  a = b;
+  b = temp;
 }
 
-void washedUp(HWND &window) {
+bool isInContact2D(int startX, int startY, int endX, int endY, int objStartX, int objStartY, int objEndX,
+                   int objEndY) {
+
+  if (startX > objStartX && startX < objEndX && startY > objStartY &&
+      startY < objEndY)
+    return true;
+  if (endX > objStartX && endX < objEndX && endY > objStartY &&
+      endY < objEndY)
+    return true;
+  if (startX > objStartX && startX < objEndX && endY > objStartY &&
+      endY < objEndY)
+    return true;
+  if (endX > objStartX && endX < objEndX && startY > objStartY &&
+      startY < objEndY)
+    return true;
+  if (startX < objStartX && objStartX < objEndX && objEndX < endX)
+    return true;
+
+  if (isInContact1D(startX, endX, objStartX) &&
+          isInContact1D(startY, endY, objStartY) ||
+      isInContact1D(startX, endX, objEndX) &&
+          isInContact1D(startY, endY, objEndY))
+    return true;
+
+  return false;
+}
+
+void washedUp(HWND& window) {
 
   HDC hdc = GetDC(window);
-
+  
   size = buffer_height / 6;
 
-  int trashPerRound = 5;
+  int trashPerRound = 1;
   std::vector<Sprite> sprites(trashPerRound);
 
   int score = 0;
@@ -217,8 +227,8 @@ void washedUp(HWND &window) {
       DispatchMessage(&message);
     }
     }
-    // Check whether all trash has been picked up, if so
-    // spawn more.
+    //Check whether all trash has been picked up, if so
+    //spawn more.
     if (sprites.empty()) {
       for (auto i = 0; i < trashPerRound; i++) {
         Sprite spr;
@@ -230,7 +240,7 @@ void washedUp(HWND &window) {
       beachCoordinates = speed;
     if (beachCoordinates >= 92)
       beachCoordinates = -700;
-    // This drawSprite function draws the background (beach).
+    //This drawSprite function draws the background (beach).
     drawSprite(beachCoordinates - speed,
                buffer_height - (100 * (buffer_width) / 200) -
                    (55 * (buffer_width - 38) / 1098),
@@ -239,18 +249,14 @@ void washedUp(HWND &window) {
     if (!sprites.empty()) {
       for (auto i = 1; i <= sprites.size() * 2; i += 2)
         draw_rect(buffer_width / 20 * 19, buffer_height / 4 + i * 20,
-                  buffer_width / 20 * 19 + 20, buffer_height / 4 + 20 + i * 20,
-                  0x0000ff);
+                  buffer_width / 20 * 19 + 20,
+                  buffer_height / 4 + 20 + i * 20, 0x0000ff);
     }
 
-    if (pressed(BUTTON_LEFT))
-      playerX -= speed;
-    if (pressed(BUTTON_RIGHT))
-      playerX += speed;
-    if (pressed(BUTTON_DOWN))
-      playerY -= speed;
-    if (pressed(BUTTON_UP))
-      playerY += speed;
+    if (pressed(BUTTON_LEFT)) playerX -= speed;
+    if (pressed(BUTTON_RIGHT)) playerX += speed;
+    if (pressed(BUTTON_DOWN)) playerY -= speed;
+    if (pressed(BUTTON_UP)) playerY += speed;
 
     if (playerY < 0)
       playerY = 0;
@@ -271,6 +277,9 @@ void washedUp(HWND &window) {
     }
 
     for (auto i = 0; i < sprites.size(); i++) {
+      sprites[i].setStartX(
+          randomNumber(playerX - buffer_width / 4, playerX + buffer_width));
+      sprites[i].setStartY(randomNumber(0, buffer_height / 5 * 3));
       auto randNum = randomNumber(0, 2);
       switch (randNum) {
       case 0:
@@ -283,28 +292,64 @@ void washedUp(HWND &window) {
         sprites[i].setName("RustedBlockOfIron");
         break;
       }
-      sprites[i].setStartX(
-          randomNumber(playerX - buffer_width / 4, playerX + buffer_width));
-      sprites[i].setStartY(randomNumber(0, buffer_height / 5 * 3));
       sprites[i].wasteCoords = wasteCoordinatesX;
       auto pointsForPickup = sprites[i].draw();
 
-      /* draw_text(hdc, "PX is " + str(playerX + buffer_width / 4), 100, 100, 3,
-                   0, 0xffffff);
-      draw_text(hdc, "PY is " + str(playerY), 400, 100, 3, 0, 0xffffff);
-      draw_text(hdc, "WX is " + str(sprites[i].startX + wasteCoordinatesX), 100,
-                125 * (i+1), 3, 0, 0xffffff);
-      draw_text(hdc, "WY is " + str(sprites[i].startY), 100,
-                150 * (i+1), 3, 0, 0xffffff); */
+      /*
 
-      /* The <= 50 is a convience feature. It makes it so that
-         the item would still collect if you were +- 50 pixels away from its
-         hitbox */
-      if (abs((playerX + buffer_width / 4) -
-                  (sprites[i].startX + wasteCoordinatesX) <=
-              50) &&
-          abs(playerY - sprites[i].startY) <= 25) {
-        if (pressed(BUTTON_ENTER)) {
+      draw_text(hdc, "startX is " + str(playerX + buffer_width / 4), 100, 100, 3, 0,
+                0xffffff);
+      draw_text(hdc, "startY is " + str(playerY), 400, 100, 3, 0, 0xffffff);
+      draw_text(hdc, "endX is " + str(playerX + buffer_width / 4 + size), 100, 80,
+                3, 0, 0xffffff);
+      draw_text(hdc, "endY is " + str(playerY + size), 400, 80, 3, 0, 0xffffff);
+      draw_text(hdc, "objStartX is " + str(sprites[i].startX + wasteCoordinatesX), 100,
+                140 * (i + 1), 3, 0, 0xffffff);
+      draw_text(hdc, "objStartY is " + str(sprites[i].startY), 400, 140 * (i + 1), 3,
+                0, 0xffffff);
+      draw_text(hdc,
+                "objEndX is " + str(sprites[i].endX + wasteCoordinatesX),
+                100, 120 * (i + 1), 3, 0, 0xffffff);
+      draw_text(hdc, "objEndY is " + str(sprites[i].endY), 400,
+                120 * (i + 1), 3, 0, 0xffffff);
+
+      // DBG hitbox of sprite
+      draw_rect(sprites[i].startX + wasteCoordinatesX, sprites[i].endY,
+                sprites[i].startX + wasteCoordinatesX + 10,
+                sprites[i].endY + 10, 0x00ff00);
+      draw_rect(sprites[i].endX + wasteCoordinatesX, sprites[i].endY,
+                sprites[i].endX + wasteCoordinatesX + 10, sprites[i].endY + 10,
+                0x00ff00);
+      draw_rect(sprites[i].endX + wasteCoordinatesX, sprites[i].startY,
+                sprites[i].endX + wasteCoordinatesX + 10,
+                sprites[i].startY + 10, 0x00ff00);
+      draw_rect(sprites[i].startX + wasteCoordinatesX, sprites[i].startY,
+                sprites[i].startX + wasteCoordinatesX + 10,
+                sprites[i].startY + 10, 0x00ff00);
+      // DBGover
+
+      // DBG hitbox of player
+      draw_rect(playerX + buffer_width / 4, playerY + size,
+                playerX + buffer_width / 4 + 10, playerY + size + 10,
+                0x000000); // Top Left
+      draw_rect(playerX + buffer_width / 4 + size, playerY + size,
+                playerX + buffer_width / 4 + 10 + size, playerY + size + 10,
+                0x000000); // Top Right
+      draw_rect(playerX + buffer_width / 4, playerY,
+                playerX + buffer_width / 4 + 10, playerY + 10,
+                0x000000); // Bottom Left
+      draw_rect(playerX + buffer_width / 4 + size, playerY,
+                playerX + buffer_width / 4 + 10 + size, playerY + 10,
+                0x000000); // Bottom Right
+      // DBGOver
+      */
+
+      if (pressed(BUTTON_ENTER)) {
+        if (isInContact2D(
+                playerX + buffer_width / 4, playerY,
+                playerX + buffer_width / 4 + size, playerY + size,
+                sprites[i].startX + wasteCoordinatesX, sprites[i].startY,
+                sprites[i].endX + wasteCoordinatesX, sprites[i].endY)) {
           score += pointsForPickup;
           erase_element_at_pos(sprites, i);
           i--;
@@ -312,8 +357,9 @@ void washedUp(HWND &window) {
       }
     }
 
-    draw_text(hdc, str(score), buffer_width / 10 * 9, (buffer_height / 10 * 9),
-              buffer_width / 300, 0, 0xffffff);
+    draw_text(hdc, str(score), buffer_width / 10 * 9,
+              (buffer_height / 10 * 9), buffer_width / 300, 0,
+              0xffffff);
 
     StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width,
                   buffer_height, buffer_memory, &buffer_bitmap_info,
